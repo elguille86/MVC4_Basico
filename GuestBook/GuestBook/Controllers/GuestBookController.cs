4836e4138f09bf3283fd6e96bba14c07d8f90300
unknown <grodriguez@INFORMATICA09.diresacallao.gob.pe>
2014-03-29 16:11:53 -0500

    Privera version de Aplicativo

---
+++ GuestBook/GuestBook/Controllers/GuestBookController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuestBook.Models;
+
+namespace GuestBook.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        //
+        // GET: /GuestBook/
+        private GuestBookContext ctx = new GuestBookContext();
+
+        public ActionResult index() {
+            //Listando los 20 primeros
+            //uso de linQ
+            var entries = (from e in ctx.Entries orderby e.DateAdded descending select e).Take(20); 
+            ViewBag.Entries = entries.ToList();
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(GuestBookEntry entry) {
+            entry.DateAdded = DateTime.Now;
+            ctx.Entries.Add(entry);
+            ctx.SaveChanges();
+           // return Content("La entrada se envio Correctamente");
+            // Se cambia para rediccion a la vista index
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ GuestBook/GuestBook/Models/GuestBookContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace GuestBook.Models
+{
+    public class GuestBookContext: DbContext
+    {
+        public GuestBookContext(): base("GuestBook")
+        {
+        
+        }
+        public DbSet<GuestBookEntry> Entries { get; set; }
+    }
+}
